0a1efc10a92e33cecb14bc20786c4f0a864990f2
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-05-04 12:37:15 +0000

    Update 3 files

--- src/Component/TodoList/TodoList.jsx
+++ src/Component/TodoList/TodoList.jsx
@@ -9,7 +9,7 @@ const TodoList = ({ todos, setTodos }) => {
         {todos.map((todo) => (
           <Todo
             text={todo.text}
-            key={todo.id}
+            id={todo.id}
             todos={todos}
             todo={todo}
             setTodos={setTodos}
